f9830e0d889939175e6f42c9a5bd86120a8d9572
TCS-ADMIN\y-itoh <y-itoh@tcs-s.co.jp>
2025-09-02 10:15:56 +0900

    プロジェクト ファイルを追加します。

---
+++ CSharpCodeRecipes/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharpCodeRecipes
+{
+    /// <summary>
+    /// エントリークラス
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// エントリーポイント
+        /// </summary>
+        /// <param name="args">コマンドライン引数</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
